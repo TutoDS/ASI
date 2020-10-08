ee7088608e12cdd4d97c2746a4d6362ea835899f
Daniel Sousa <jdaniel.asousa@gmail.com>
2020-10-08 22:16:28 +0100

    Add example of tuple

--- Python/exemplos.py
+++ Python/exemplos.py
@@ -1,5 +1,17 @@
 import json
 
+# Tuplos
+isMyTuple = (12, 20, 30)
+
+print(isMyTuple)
+
+"""
+isMyTuple[0] = 20
+
+Appear error because on tuples is not possible change value
+Useful to months of year (for example)
+"""
+
 # Dicionários
 # Chave única
 alunos = {"8160334": {"nome": "Daniel Sousa", "curso": "LEI"}, "818044": {"nome": "Luis", "curso": "LSIRC"}}
