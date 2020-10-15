de7e8d87f892d0ffd7201abe1eee45af80338c82
Daniel Sousa <jdaniel.asousa@gmail.com>
2020-10-15 15:48:30 +0100

    Fix exercises

--- Python/exercicios/aula_02/exercicio-2.py
+++ Python/exercicios/aula_02/exercicio-2.py
@@ -1,14 +1,10 @@
 notes = [8, 7, 8, 6, 10, 12, 14, 12, 18, 12, 17]
 
 
-def countNumber(number, list):
-    return "A nota %d aparece %d vezes" % (number, list.count(number))
-
-
 def listOfNotes():
     #  21 => 0 to 20
     for num in range(21):
-        print(countNumber(num, notes))
+        print("A nota %d aparece %d vezes" % (num, notes.count(num)))
 
 
 listOfNotes()
--- Python/exercicios/aula_02/exercicio-3.py
+++ Python/exercicios/aula_02/exercicio-3.py
@@ -8,22 +8,23 @@ def stripDate(date):
 
 
 # Classifica a idade
-def classifyAge(age):
-    return "Criança" if age <= 12 else "Juvenil" if age >= 13 and age <= 17 else "Adulto" if age >= 18 and age <= 64 else "Sénior"
+def classifyAge(birthayDate):
+    age = calcAge(birthayDate)
+    return "Criança" if age <= 12 else "Juvenil" if 13 <= age <= 17 else "Adulto" if 18 <= age <= 64 else "Sénior"
 
 
 # Calcular Idade (com base no ano)
-def calcAge(year):
-    return int(str(date.today()).split('-')[0]) - int(year)
+def calcAge(birthayDate):
+    return int(str(date.today()).split('-')[0]) - int(stripDate(birthayDate))
 
 
 # Ages
-persons = ['12/12/1995', '01/03/2010', '10/01/2013']
+persons = ['12/12/1995', '01/03/2010', '10/01/2013', "10/01/2007"]
 
 # Imprime idade e classificação
 for person in persons:
     print("Data de Nascimento %s %d %s" % (
         person + ": tem",
-        calcAge(stripDate(person)),
-        "anos, e é " + classifyAge(calcAge(stripDate(person)))
+        calcAge(person),
+        "anos, é " + classifyAge(person)
     ))
