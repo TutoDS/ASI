181dbe51e034960cde92f30a583a17ad5d848bae
Daniel Sousa <jdaniel.asousa@gmail.com>
2020-10-15 14:20:05 +0100

    Second version of exercise 2

--- Python/exercicios/aula_02/exercicio-2.py
+++ Python/exercicios/aula_02/exercicio-2.py
@@ -1,22 +1,15 @@
+notes = [8, 7, 8, 6, 10, 12, 14, 12, 18, 12, 17]
+
+
 def countNumber(number, myList):
     nTimes = myList.count(number)
     return "O número %d" % number + " aparece %d" % nTimes + " vezes"
 
 
-def listLoop(list):
-    nonDuplicatedList = []
-
-    for num in list:
-        if num not in nonDuplicatedList:
-            nonDuplicatedList.append(num)
-
-    for number in nonDuplicatedList:
-        print(countNumber(number, list))
+def listOfNotes():
+    #  21 => 0 to 20
+    for num in range(21):
+        print(countNumber(num, notes))
 
 
-notes = [8, 7, 8, 6, 10, 12, 14, 12, 18, 12, 17]
-notes.sort()
-
-print("Notas ordenadas: ", notes)
-
-print(listLoop(notes))
+print(listOfNotes())
