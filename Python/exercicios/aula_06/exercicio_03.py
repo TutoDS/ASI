a28e7fbe14b8c0377aa99dee28c9886b79bf4871
Daniel Sousa <jdaniel.asousa@gmail.com>
2020-11-12 14:48:01 +0000

    Update classe 6 exercises

--- Python/exercicios/aula_06/exercicio_03.py
+++ Python/exercicios/aula_06/exercicio_03.py
@@ -1,23 +1,18 @@
 import re
 
-regex = re.compile(r"(?P<product>.*):\s(?P<price>.*)")
-regexPrice = re.compile("(?P<symbol>\$|€)(\d*\.?\d*)")
-breaklinePattern = "\\n"
-priceSymbolPattern = "\$|€"
+regex = re.compile("^(?P<product>.*):\s\$(?P<price>.*)$")
 
 
 def convertPrice(price):
     priceConverted = float(price) * 0.8182
-    return priceConverted
+    return round(priceConverted, 2)
 
 
 with open("./enunciados/products.txt") as file:
     for line in file:
-        price = regex.sub(r"\g<price>", line)
-        price = re.sub(breaklinePattern, '', price)  # remove \n
-        symbol = regexPrice.sub(r"\g<symbol>", price)  # symbol
 
-        if symbol != "€":
-            print("%s em euros é %f €" % (price, convertPrice(re.sub(priceSymbolPattern, '', price))))
-        else:
-            print("O valor já %s já se encontra em euros!" % (price))
+        lista = regex.match(line)
+
+        if lista:
+            price = convertPrice(lista.group(2))
+            print(lista.group("product") + ": " + str(price) + " €")
